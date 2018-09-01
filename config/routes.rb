6af6b78ed375a2be89d03f05dfc64ba97bab0809
Julie E <julie.eubank@gmail.com>
2018-08-31 19:15:27 -0700

    Adding guests and messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'message/index'
+  get 'guests', to: 'guests#index'
+  resources :guests
+
+  resources :message
+  get 'messages', to: 'message#index'
+  get 'home', to: 'home#index'
   get 'welcome/index'
   root 'welcome#index'
   # devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_210010) do
+ActiveRecord::Schema.define(version: 2018_09_01_014206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "guests", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "first_name", default: "", null: false
+    t.string "phone", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_guests_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "user_id"
+    t.text "content", default: "", null: false
+    t.datetime "scheduled_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
